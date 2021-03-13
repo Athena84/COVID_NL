43d3c971476c59547c5eca5f72d8de49e1e16f16
Athena84 <n.a.w.kreuk@gmail.com>
2021-03-13 15:03:54 +0100

    fixed library issue

--- global.R
+++ global.R
@@ -5,11 +5,10 @@ library(shiny)
 library(ggplot2) #for plotting
 library(dplyr) #for last steps data sorting
 library(sf) #for maps
-#library(shinythemes)
-#library(viridisLite)
-#(RColorBrewer)
+library(shinythemes)
+library(RColorBrewer) #for gradient chart colours
 library(rsconnect) #for publishing
-#rsconnect::deployApp('path/to/your/app')
+#rsconnect::deployApp('https://athena84.shinyapps.io/COVID_NL/')
 
 #Load datasets (pre-processing of raw data in datacleaner.R)
 vacc_will_subset <- read.csv("./Data/vacc_will.csv", stringsAsFactors = FALSE, header = TRUE)
